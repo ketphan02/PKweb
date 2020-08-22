2fe5dbeaf26a0d03dc66481b641f90a5a90d850f
phankiet02 <tuankietvn@outlook.com>
2020-08-22 15:43:54 +0700

    Updates

---
+++ precache-manifest.225a5878e5d8917760e5cd4699061531.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e27c3076446ecc903377cf4d90791a",
+    "url": "/PKweb/index.html"
+  },
+  {
+    "revision": "24ba772d17e26309ec65",
+    "url": "/PKweb/static/css/main.bf7dece4.chunk.css"
+  },
+  {
+    "revision": "d83277020b39fc26b3b6",
+    "url": "/PKweb/static/js/2.5feef017.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PKweb/static/js/2.5feef017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ba772d17e26309ec65",
+    "url": "/PKweb/static/js/main.a3548a1d.chunk.js"
+  },
+  {
+    "revision": "d829c8f864614238342f",
+    "url": "/PKweb/static/js/runtime-main.44779f00.js"
+  },
+  {
+    "revision": "093a6e3f27aab5f3bc298746976e99f3",
+    "url": "/PKweb/static/media/profile.093a6e3f.png"
+  },
+  {
+    "revision": "e24aef9754623a114ba519e49b654231",
+    "url": "/PKweb/static/media/school-24px.e24aef97.svg"
+  }
+]);
